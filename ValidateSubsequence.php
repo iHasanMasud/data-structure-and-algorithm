9301ee7ea706de7ed9c529333cbd66c0720c441a
Hasan Masud <hasan.masud.dcc@gmail.com>
2023-03-25 01:56:55 +0600

    Validate Subsequence

---
+++ ValidateSubsequence.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Validate Subsequence
+ * Author: https://github.com/iHasanMasud
+ */
+
+function isValidSubsequence($array, $sequence){
+    $seqIdx = 0;
+    foreach ($array as $item) {
+        if ($seqIdx == count($sequence)) {
+            break;
+        }
+        if ($item == $sequence[$seqIdx]) {
+            $seqIdx++;
+        }
+    }
+    return $seqIdx == count($sequence);
+}
+
+print_r(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10]));
